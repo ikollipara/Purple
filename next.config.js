487aa7f449a96e8de1113d6434930c605fa6b80f
Ian Kollipara <ian.kollipara@cune.org>
2022-10-18 23:09:07 -0500

    Fixed Homepage

--- next.config.js
+++ next.config.js
@@ -9,4 +9,11 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  async redirects() {
+    return {
+      source: "/",
+      destination: "/lessons",
+      permanent: true,
+    };
+  },
 });

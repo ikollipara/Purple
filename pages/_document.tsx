409484ff084600161aef6673b6b22d916dfecaf2
Ian Kollipara <ian.kollipara@cune.org>
2022-11-24 08:50:37 -0600

    Added SEO Tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="emerald">
-      <Head></Head>
+      <Head>
+        <meta
+          name="description"
+          content="A collection of lesson plans for children in Seward Nebraska to get them outside and in nature."
+        />
+        <meta
+          name="keywords"
+          content="Nature, Nebraska, Concordia, Cune, Seward, Lesson, Outside"
+        />
+        <meta name="author" content="Rebecca Yox" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <Main />
         <NextScript />
